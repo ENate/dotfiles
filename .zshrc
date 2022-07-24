b021403d25d928ecc26f3008b07a7195d2fb4e0c
Nathaniel Egwu <nathaniel@aims.ac.za>
2022-07-24 17:44:18 +0000

    Changes zshrc config theme

--- .zshrc
+++ .zshrc
@@ -1,6 +1,8 @@
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="agnoster"
+#ZSH_THEME="agnoster"
+ZSH_THEME=random
+ZSH_THEME_RANDOM_CANDIDATES=("agnoster" "powerlevel9k/powerlevel9k")
 
 # zsh-nvm and zsh-autosuggestions must be installed separately
 plugins=(git zsh-nvm zsh-autosuggestions)
--- install.sh
+++ install.sh
@@ -10,8 +10,14 @@ zshrc() {
     echo "-----------------------------------------------------------"
     git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
     echo "==========================================================="
+    echo "                cloning powerlevel9k                       "
+    echo "-----------------------------------------------------------"
+    $ git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
+    echo "==========================================================="
+
     echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
+    
     cat .zshrc > $HOME/.zshrc
 }
 
